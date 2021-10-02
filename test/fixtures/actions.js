9b61f9c418dde1cecad700f04431389f8181c8ab
Luca Lanziani <luca@lanziani.com>
2021-10-02 21:30:00 +0200

    Fail if even one template is invalid
    Test via persit method

--- test/fixtures/actions.js
+++ test/fixtures/actions.js
@@ -1,63 +1,49 @@
 module.exports = {
-  invalid: [
+  'org1/repo1': [
     {
+      template: 'test.txt',
       destination: {
         type: 'tpd-github',
         params: {
-          repo: 'invalid',
-          filepath: 'deep/folder/multiple/levels/text.txt',
+          repo: 'org1/repo1',
+          filepath: 'file1.txt',
+        },
+      },
+      renderedTemplate: 'test',
+    },
+    {
+      template: 'test.txt',
+      destination: {
+        type: 'tpd-github',
+        params: {
+          repo: 'org1/repo1',
+          filepath: 'existingDir/text.txt',
         },
       },
       renderedTemplate: 'test',
     },
   ],
-  by_repo: {
-    'org1/repo1': [
-      {
-        template: 'test.txt',
-        destination: {
-          type: 'tpd-github',
-          params: {
-            repo: 'org1/repo1',
-            filepath: 'file1.txt',
-          },
-        },
-        renderedTemplate: 'test',
-      },
-      {
-        template: 'test.txt',
-        destination: {
-          type: 'tpd-github',
-          params: {
-            repo: 'org1/repo1',
-            filepath: 'existingDir/text.txt',
-          },
-        },
-        renderedTemplate: 'test',
-      },
-    ],
-    'org2/repo1': [
-      {
-        template: 'test.txt',
-        destination: {
-          type: 'tpd-github',
-          params: {
-            repo: 'org2/repo1',
-            filepath: 'deep/folder/multiple/levels/text.txt',
-          },
-        },
-        renderedTemplate: 'test',
-      },
-      {
-        template: null,
-        destination: {
-          type: 'tpd-github',
-          params: {
-            repo: 'org2/repo1',
-            filepath: 'deep/folder/multiple/levels/old.txt',
-          },
+  'org2/repo1': [
+    {
+      template: 'test.txt',
+      destination: {
+        type: 'tpd-github',
+        params: {
+          repo: 'org2/repo1',
+          filepath: 'deep/folder/multiple/levels/text.txt',
         },
       },
-    ],
-  },
+      renderedTemplate: 'test',
+    },
+    {
+      template: null,
+      destination: {
+        type: 'tpd-github',
+        params: {
+          repo: 'org2/repo1',
+          filepath: 'deep/folder/multiple/levels/old.txt',
+        },
+      },
+    },
+  ],
 };
